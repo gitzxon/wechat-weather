3b13bae4d4e59a3c184ba028dff9bcbd72c99cde
gitzxon <tjuang@tju.edu.cn>
2016-01-27 18:09:34 +0800

    Update db.php

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '218003',
+    'password' => '',
     'charset' => 'utf8',
 ];
